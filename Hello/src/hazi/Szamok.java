627102c8813e39d3f6806767b76ac3f7f52c351e
SindelyRichard <rsindely@gmail.com>
2024-02-14 22:26:58 +0100

    Első gyakorlat házi feladat

---
+++ Hello/src/hazi/Szamok.java
@@ -0,0 +1,13 @@
+package hazi;
+
+public class Szamok {
+	public static void main(String[] args) {
+		System.out.println("Házi feladat. 2.feladat");
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i);
+			}
+			System.out.print("\n");
+		}
+	}
+}
